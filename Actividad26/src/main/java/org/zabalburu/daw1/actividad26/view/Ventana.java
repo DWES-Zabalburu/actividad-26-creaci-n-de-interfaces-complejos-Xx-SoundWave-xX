39197561813a74122952d80be57bfdd51298d896
Xx-SoundWave-xX <fernandojohel.lara.mantilla@zabalburu.org>
2025-04-08 13:41:13 +0200

    ACTIVIDAD24

--- Actividad26/src/main/java/org/zabalburu/daw1/actividad26/view/Ventana.java
+++ Actividad26/src/main/java/org/zabalburu/daw1/actividad26/view/Ventana.java
@@ -7,9 +7,11 @@ package org.zabalburu.daw1.actividad26.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -24,15 +26,25 @@ public class Ventana extends JFrame{
     private Dimension dmVentana = new Dimension(700,500);
     
     private JLabel lblTitulo = new JLabel("Actualiza tu Perfil");
+    
+    private JLabel lblNombre = new JLabel("Nombre Completo");
     private JTextField txtNombre = new JTextField();
+    private JLabel lblCorreo = new JLabel("Correo Electronico");
     private JTextField txtCorreo = new JTextField();
+    private JLabel lblTelefono = new JLabel("Telefono");
     private JTextField txtTelefono = new JTextField();
+    private JLabel lblDireccion = new JLabel("Direccion");
     private JTextField txtDireccion = new JTextField();
+    private JLabel lblSitioWeb = new JLabel("Sitio Web");
     private JTextField txtSitioWeb = new JTextField();
     
+    private JButton btnGuardarCambios = new JButton("Guardar Cambios");
+    private JButton btnCancelar = new JButton("Cancelar");
     
     
-    private JPanel pnlDatos = new JPanel(new GridLayout(5,1,0,10));
+    
+    private JPanel pnlDatos = new JPanel(new GridLayout(10,1,5,10));
+    private JPanel pnlBotones = new JPanel(new FlowLayout(FlowLayout.RIGHT));
     
     public Ventana(){
         this.setSize(dmVentana);
@@ -40,23 +52,41 @@ public class Ventana extends JFrame{
         this.add(lblTitulo , BorderLayout.NORTH);
         lblTitulo.setBackground(new Color(48,132,188));
         lblTitulo.setOpaque(true);
-        lblTitulo.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 10));
+        lblTitulo.setBorder(BorderFactory.createEmptyBorder(10, 30, 10, 30));
         lblTitulo.setFont(new Font("Calibri",Font.BOLD,22));
         lblTitulo.setForeground(Color.white);
         
-        
+        pnlDatos.add(lblNombre);
         pnlDatos.add(txtNombre);
         txtNombre.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(48,132,188)));
+        pnlDatos.add(lblCorreo);
         pnlDatos.add(txtCorreo);
         txtCorreo.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(48,132,188)));
+        pnlDatos.add(lblTelefono);
         pnlDatos.add(txtTelefono);
         txtTelefono.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(48,132,188)));
+        pnlDatos.add(lblDireccion);
         pnlDatos.add(txtDireccion);
         txtDireccion.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(48,132,188)));
+        pnlDatos.add(lblSitioWeb);
         pnlDatos.add(txtSitioWeb);
         txtSitioWeb.setBorder(BorderFactory.createMatteBorder(0, 0, 3, 0, new Color(48,132,188)));
         pnlDatos.setBorder(BorderFactory.createEmptyBorder(0,20,10,20));
         this.add(pnlDatos, BorderLayout.CENTER);
+        
+        
+        btnCancelar.setFont(new Font("Calibri",Font.BOLD,14));
+        btnCancelar.setBackground(new Color(192,196,196));
+        btnCancelar.setForeground(Color.white);
+        btnCancelar.setBorder(BorderFactory.createEmptyBorder(10,20,5,20));
+        btnGuardarCambios.setFont(new Font("Calibri",Font.BOLD,14));
+        btnGuardarCambios.setBackground(new Color(48,132,188));
+        btnGuardarCambios.setForeground(Color.white);
+        btnGuardarCambios.setBorder(BorderFactory.createEmptyBorder(10,20,5,20));
+        pnlBotones.add(btnCancelar);
+        pnlBotones.add(btnGuardarCambios);
+        pnlBotones.setBorder(BorderFactory.createEmptyBorder(0,0,0,20));
+        this.add(pnlBotones, BorderLayout.SOUTH);
         
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
